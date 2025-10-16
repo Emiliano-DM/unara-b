8b700453eac583fb40d0a299553c70a6ac1b5f06
Emiliano-DM <e.dimascolo2024@gmail.com>
2025-10-16 14:03:17 +0200

    feat: implement survey CRUD and voting logic

--- src/surveys/dto/create-survey.dto.ts
+++ src/surveys/dto/create-survey.dto.ts
@@ -1,1 +1,41 @@
-export class CreateSurveyDto {}
+import { Type } from 'class-transformer';
+import { IsString, IsEnum, IsBoolean, IsOptional, IsArray, ValidateNested, ArrayMinSize } from 'class-validator';
+
+class CreateSurveyOptionDto {
+    @IsString()
+    text: string;
+
+    @IsOptional()
+    latitude?: number;
+
+    @IsOptional()
+    longitude?: number;
+
+    @IsOptional()
+    @IsString()
+    placeId?: string;
+
+    @IsOptional()
+    datetime?: Date;
+}
+
+export class CreateSurveyDto {
+    @IsString()
+    question: string;
+
+    @IsEnum(['Destino', 'Fechas', 'General'])
+    category: string;
+
+    @IsEnum(['text', 'location', 'datetime'])
+    data_type: string;
+
+    @IsBoolean()
+    @IsOptional()
+    multiple_choice?: boolean;
+
+    @IsArray()
+    @ArrayMinSize(2)
+    @ValidateNested({ each: true })
+    @Type(() => CreateSurveyOptionDto)
+    options: CreateSurveyOptionDto[];
+}
--- src/surveys/surveys.service.ts
+++ src/surveys/surveys.service.ts
@@ -1,15 +1,66 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { CreateSurveyDto } from './dto/create-survey.dto';
 import { UpdateSurveyDto } from './dto/update-survey.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Survey, SurveyOption, SurveyVote } from './entities';
+import { Repository } from 'typeorm';
+import { Trip } from 'src/trips/entities/trip.entity';
+import { User } from 'src/users/entities/user.entity';
+
 
 @Injectable()
 export class SurveysService {
-  create(createSurveyDto: CreateSurveyDto) {
-    return 'This action adds a new survey';
+  constructor(
+    @InjectRepository(Survey)
+    private readonly surveyRepository: Repository<Survey>,
+    
+    @InjectRepository(SurveyOption)
+    private readonly optionRepository: Repository<SurveyOption>,
+    
+    @InjectRepository(SurveyVote)
+    private readonly voteRepository: Repository<SurveyVote>,
+    
+    @InjectRepository(Trip)
+    private readonly tripRepository: Repository<Trip>,
+    
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+
+  async create(tripId: string, userId: string, createSurveyDto: CreateSurveyDto) {
+    const trip = await this.tripRepository.findOne({
+      where: {id:tripId}, 
+      relations: {users:true}
+    })
+    if (!trip){
+      throw new NotFoundException('Trip not found')
+    }
+    if (!trip.users.some(u=> u.id === userId)){
+      throw new UnauthorizedException('Not a trip participant')
+    }
+    const user = await this.userRepository.findOneBy({id:userId})
+    if(!user){
+      throw new UnauthorizedException('Invalid user')
+    }
+
+    const survey = this.surveyRepository.create({
+        ...createSurveyDto,
+        trip,
+        user,
+        options : createSurveyDto.options.map(opt => this.optionRepository.create(opt))
+    })
+
+    await this.surveyRepository.save(survey)
+    return survey
   }
 
-  findAll() {
-    return `This action returns all surveys`;
+  async findAll(tripId:string) {
+    return await this.surveyRepository.find({
+      where: { trip: { id: tripId } },
+      relations: { options: true, user: true },
+      order: { created_at: 'DESC' }
+    });;
   }
 
   findOne(id: number) {
@@ -22,5 +73,38 @@ export class SurveysService {
 
   remove(id: number) {
     return `This action removes a #${id} survey`;
+  }
+
+  async vote(surveyId:string, userId:string, optionId:string){
+    const survey = await this.surveyRepository.findOneBy({id:surveyId})
+    if (!survey){
+      throw new NotFoundException('survey not found')
+    }
+    if (survey.closed){
+      throw new BadRequestException('survey already closed')
+    }
+
+    const user = await this.userRepository.findOneBy({id: userId})
+    if (!user){
+      throw new UnauthorizedException('Invalid user')
+    }
+
+    const existingVote = await this.voteRepository.findOne({
+      where : {survey:{id:surveyId}, user: {id:userId}}
+    })
+    if (existingVote){
+      throw new BadRequestException('Already voted')
+    }
+    
+    const option = await this.optionRepository.findOneBy({id:optionId})
+    if (!option){
+      throw new NotFoundException('Option not found')
+    }
+    const vote = this.voteRepository.create({survey, option, user})
+    option.votes += 1
+    await this.optionRepository.save(option)
+    await this.voteRepository.save(vote)
+    return vote
+
   }
 }
